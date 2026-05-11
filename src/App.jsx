bab73a9577dd38c6b483dfc86a5061da15286073
MarcoZ23 <zollomarco23@gmail.com>
2026-05-11 21:43:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+
+function App() {
+
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+  const [genere, setGenere] = useState("")
+  console.log(genere);
+
+
+  return (
+    <>
+      <select className="form-select" aria-label="Default select example"
+        onChange={(e) => setGenere(e.target.value)}>
+        <option value="">Select a genre</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+
+      </select>
+    </>
+  )
+}
+
+export default App
+
+{/* <ul className="">
+  {movies.map(movie => (
+    <li key={movie.index}>{movie.title}</li>
+  ))}
+</ul> */}
